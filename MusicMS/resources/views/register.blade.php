d31981004a5bb7ab242e99d9fa65b3b47b444b2b
Vincentius Loanka Sinaga <70961951+XiaoLongEr8@users.noreply.github.com>
2022-12-17 12:04:43 +0700

    Feat: Register Page

--- MusicMS/resources/views/register.blade.php
+++ MusicMS/resources/views/register.blade.php
@@ -27,12 +27,52 @@
             <section class="title_container">
                 <h1 class="regis_title">Lyrics-Go</h1>
             </section>
+
             <form action="">
+
+                <section class="input_Row_Section">
+                    <div class="input_Column_Section">
+                        <label for="full_name" class="item_Title">Full Name</label>
+                        <input type="text" class="full_Name" id="full_Name_Value" placeholder="Full Name" required>
+                    </div>
+                </section>
+
+                <section class="input_Row_Section">
+                    <div class="input_Column_Section">
+                        <label for="username" class="item_Title">Username</label>
+                        <input type="text" class="username" id="username" placeholder="Username" required>
+                    </div>
+                </section>
+
                 <section class="input_Row_Section">
                     <div class="input_Column_Section">
                         <label for="email" class="item_Title">Email</label>
                         <input type="email" class="email" id="email_Value" placeholder="Email"
                             aria-describedby="emailHelp" required>
+                    </div>
+                </section>
+
+                <section class="input_Row_Section">
+                    <div class="input_Column_Section">
+                        <label for="phone">Phone Number</label>
+                        <input type="tel" id="phone" name="phone"
+                            pattern="^(^\+62|62|^08)(\d{3,4}-?){2}\d{3,4}$" placeholder="Phone Number" required>
+                    </div>
+                </section>
+
+                <section class="input_Row_Section">
+                    <div class="input_Column_Section">
+                        <label for="pass">Password</label>
+                        <input type="password" id="pass" placeholder="Password" name="password" minlength="8"
+                            required>
+                    </div>
+                </section>
+
+                <section class="input_Row_Section">
+                    <div class="input_Column_Section">
+                        <label for="pass">Confirm Password</label>
+                        <input type="password" id="con_pass" name="password" placeholder="Confirm Password"
+                            minlength="8" required>
                     </div>
                 </section>
 
