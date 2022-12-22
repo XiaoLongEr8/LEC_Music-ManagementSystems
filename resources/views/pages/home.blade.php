fded1a50afd5a9aa797f3053b1efd93002988123
Vincentius Loanka Sinaga <70961951+XiaoLongEr8@users.noreply.github.com>
2022-12-22 11:54:31 +0700

    Feat: Slicing Hero Section
    (+) First Section (Introduction Content)
    (+) Second Section (Search Content)

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,47 @@
 @extends('layouts.guest')
 
+@section('link')
+    {{-- Css --}}
+    <link rel="stylesheet" href="css/style/home.css">
+
+    {{-- Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+@endsection
+
 @section('container')
-    <h1>This is home Page</h1>
+    <main class="main_container">
+
+        <section class="first_section">
+            <div class="left_content">
+                <h1 class="main_title">
+                    Welcome to <br>
+                    Lyrics-Go!</h1>
+                <h5 class="sub_title">
+                    Search your favorite song lyrics <br>
+                    and sing-along!</h5>
+            </div>
+
+            <picture>
+                <img src="image/listening-happy-music.png" alt="Happy Music IMG">
+            </picture>
+        </section>
+
+        <section class="second_section">
+            <div class="title_section">
+                <h2>Let's Explore the world of Music!</h2>
+            </div>
+
+            <div class="search_section">
+                <button type="" class="search_button"><img src="image/search_logo.png" alt="Search logo"></button>
+                <form action="" class="search_input">
+                    <input type="text" placeholder="Search song title, artist, or lyrics...">
+                </form>
+            </div>
+
+        </section>
+
+    </main>
 @endsection
--- resources/views/partials/upper_NavBar.blade.php
+++ resources/views/partials/upper_NavBar.blade.php
@@ -1,3 +1,15 @@
-<nav>
-    <h1>This is NavBar</h1>
+<style>
+    .upper_navbar_container {
+        background-color: salmon
+    }
+</style>
+
+<nav class="upper_navbar_container">
+    <section class="leftContent">
+        <h1>Lyrics-Go</h1>
+    </section>
+
+    <section>
+        <button class="login_btn">login</button>
+    </section>
 </nav>
