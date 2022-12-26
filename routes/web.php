22bd08b4daed6cc097fce3dc5258a39a0278771a
YouCanCallMeMatt <68661328+YouCanCallMeMatt@users.noreply.github.com>
2022-12-26 16:10:08 +0700

    Feat: Login Page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::get('/login', function () {
+    return view('pages.login');
+});
+
