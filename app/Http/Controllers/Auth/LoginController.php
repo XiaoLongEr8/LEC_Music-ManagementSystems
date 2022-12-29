a1fa1ac2ee0acccc23b141ed6cab4cbc7d56f418
greg-hugo <hugo.gregory5@gmail.com>
2022-12-29 19:37:57 +0700

    new: import package

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use Laravel\Socialite\Facades\Socialite;
 
