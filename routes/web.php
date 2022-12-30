1c7d53ab1d354ce7fe2b29f6b065429fb08e2811
greg-hugo <hugo.gregory5@gmail.com>
2022-12-30 15:09:34 +0700

    feat: finish home song api

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/result', function () {
     return view('pages.searchResult');
