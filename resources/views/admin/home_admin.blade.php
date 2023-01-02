8b5f0ea51d55a52f71f45095383f1890b7182b8c
Ignatius Michael Dinata <michaeldinata33@gmail.com>
2023-01-03 01:55:36 +0700

    feat: admin panel song list

--- resources/views/admin/home_admin.blade.php
+++ resources/views/admin/home_admin.blade.php
@@ -1,29 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/admin/admin_home.css">
-    <link rel="stylesheet" href="css/admin/side_navbar.css">
+@extends('layouts.admin')
 
-    {{-- Fonts --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+@section('pageTitleAdmin')
 
-    {{-- Icons --}}
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-</head>
-<body>
+@section('contentsAdmin')
+    <section id="AdminHome" class="admin-section">
+        <h1 class="admin-pagetitle">Song List</h1>
 
-    @include('partials.side_NavBar')
+        <table class="songtitle-table admin-table">
+            <tr class="heading-table">
+                <th class="col-1">No</th>
+                <th class="col-2">Song Title</th>
+                <th class="col-3">Artist</th>
+                <th class="col-4">Views</th>
+                <th class="col-5">Release Date</th>
+                <th class="col-6">Lyrics</th>
+                <th class="col-7">Edit</th>
+                <th class="col-8">Delete</th>
+            </tr>
 
-    {{-- <section id="AdminHome" class="admin-section">
-        adminhomepage
-    </section> --}}
-
-</body>
-</html>
+            {{-- tr dibawah nnti pake foreach --}}
+            <tr>
+                <td>1</td>
+                <td>Bitterlove</td>
+                <td>Ardhito Pramono</td>
+                <td>2.652</td>
+                <td>12/10/2019</td>
+                <td>
+                    <a href="">
+                        <button class="table-adminbutton viewlyrics">Lyrics</button>
+                    </a>
+                </td>
+                <td>
+                    <a href="">
+                        <button class="table-adminbutton editlyrics">Edit</button>
+                    </a>
+                </td>
+                <td>
+                    <a href="">
+                        <button class="table-adminbutton deletelyrics">Delete</button>
+                    </a>
+                </td>
+            </tr>
+        </table>
+    </section>
+@endsection
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('pageTitleAdmin')</title>
+    <link rel="stylesheet" href="css/admin/admin_home.css">
+    <link rel="stylesheet" href="css/admin/side_navbar.css">
+
+    {{-- Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    {{-- Icons --}}
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</head>
+<body>
+
+    @include('partials.side_NavBar')
+
+    @yield('contentsAdmin')
+
+</body>
+</html>
