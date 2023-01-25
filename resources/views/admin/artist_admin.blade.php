af65a7f0e212243d874622a9892301a9ca09d39c
Vincentius Loanka Sinaga <70961951+XiaoLongEr8@users.noreply.github.com>
2023-01-25 21:46:13 +0700

    Update artist_admin.blade.php

--- resources/views/admin/artist_admin.blade.php
+++ resources/views/admin/artist_admin.blade.php
@@ -19,31 +19,31 @@
 
 
                 @foreach ($artists as $artist)
-                <tr>
-                    <td>{{((request('page', 1)-1)*10)+$loop->index+1}}</td>
-                    <td>{{$artist->fullname}}</td>
-                    <td>{{$artist->nationality}}</td>
-                    <td>
-                        <a href="{{route('artist.show', $artist->id)}}">
-                            <button class="table-adminbutton viewlyrics">Artist Detail</button>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="{{route('redirect.edit.artist', $artist->id)}}">
-                            <button class="table-adminbutton editlyrics">Edit</button>
-                        </a>
-                    </td>
-                    <td>
-                        <form action="{{route('delete.artist', $artist->id)}}" method="POST">
-                            @csrf
-                            <button type="submit" class="table-adminbutton deletelyrics">Delete</button>
-                        </form>
-                    </td>
-                </tr>
+                    <tr>
+                        <td>{{ (request('page', 1) - 1) * 10 + $loop->index + 1 }}</td>
+                        <td>{{ $artist->fullname }}</td>
+                        <td>{{ $artist->nationality }}</td>
+                        <td>
+                            <a href="{{ route('artist.show', $artist->id) }}">
+                                <button class="table-adminbutton viewlyrics">Artist Detail</button>
+                            </a>
+                        </td>
+                        <td>
+                            <a href="{{ route('redirect.edit.artist', $artist->id) }}">
+                                <button class="table-adminbutton editlyrics">Edit</button>
+                            </a>
+                        </td>
+                        <td>
+                            <form action="{{ route('delete.artist', $artist->id) }}" method="POST">
+                                @csrf
+                                <button type="submit" class="table-adminbutton deletelyrics">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
                 @endforeach
             </table>
-            {{$artists->links()}}
-            <a href="">
+            {{ $artists->links() }}
+            <a href="/add-artist">
                 <button class="add-new-item">
                     <ion-icon name="add-outline"></ion-icon>
                     <p>Add New Artist</p>
